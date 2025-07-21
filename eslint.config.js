391d89ec25506d5ac767066484697d25e077dde3
Filip <f.grzechnik1@gmail.com>
2025-07-21 12:09:19 +0200

    Enforces Prettier formatting as an error
    Configures ESLint to report Prettier formatting issues as errors, ensuring consistent code style and preventing potential style-related conflicts.

--- eslint.config.js
+++ eslint.config.js
@@ -34,6 +34,7 @@ export default defineConfig([
       rules: {
          "react-hooks/exhaustive-deps": "warn",
          "@typescript-eslint/no-namespace": "off",
+         "eslint-plugin-prettier/prettier": "error",
       },
    },
    {
